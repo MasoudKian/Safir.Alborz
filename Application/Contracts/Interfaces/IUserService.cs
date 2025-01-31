558a46036989a3fc367663e3c8ceae7b59e61965
Masoud Kian <masoudkiannezhad@gmail.com>
2025-01-31 23:32:21 +0330

    Set IAuthService

--- Application/Contracts/Interfaces/IUserService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.Contracts.Interfaces
-{
-    public interface IUserService
-    {
-
-    }
-}
---
+++ Application/Contracts/Interfaces/Identity/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Application.Models.AuthenticationIdentity;
+
+namespace Application.Contracts.Interfaces.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> LoginAsync(AuthRequest authRequest);
+        Task<RegisterResponse> RegisterAsync(RegisterRequest authRequest);
+    }
+}
