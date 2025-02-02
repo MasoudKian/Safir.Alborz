23e0023064b810605806033e8df778aa098f000e
Masoud Kian <masoudkiannezhad@gmail.com>
2025-02-02 16:26:48 +0330

    set AuthService in web

---
+++ WEB/Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Models.AuthenticationIdentity;
+
+namespace WEB.Services
+{
+    public class AuthService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<AuthResponse> LoginAsync(AuthRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/v1/account/login", request);
+            response.EnsureSuccessStatusCode(); // خطا را پرتاب می‌کند اگر وضعیت HTTP نشان‌دهنده‌ی خطا باشد
+            return await response.Content.ReadFromJsonAsync<AuthResponse>();
+        }
+
+        public async Task<RegisterResponse> RegisterAsync(RegisterRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/v1/account/register", request);
+            response.EnsureSuccessStatusCode(); // خطا را پرتاب می‌کند اگر وضعیت HTTP نشان‌دهنده‌ی خطا باشد
+            return await response.Content.ReadFromJsonAsync<RegisterResponse>();
+        }
+    }
+}
