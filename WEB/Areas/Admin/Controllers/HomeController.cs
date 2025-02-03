0e22da9bcff1139f0dddfb686489ca2a8e1fc0a4
Masoud Kian <masoudkiannezhad@gmail.com>
2025-02-03 17:57:53 +0330

    Start Human Resources - منابع انسانی

--- WEB/Areas/Admin/Controllers/HomeController.cs
+++ WEB/Areas/Admin/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ namespace WEB.Areas.Admin.Controllers
 {
     public class HomeController : AdminBaseController
     {
+
         public IActionResult AdminHome()
         {
             return View();
