28ca8e29a1107d70007110a934612ea5c882c9bf
Masoud Kian <masoudkiannezhad@gmail.com>
2025-02-10 18:37:50 +0330

    set DepartmentRepository

---
+++ Application/Contracts/Interfaces/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.HumanResources.EmployeeManagement;
+
+namespace Application.Contracts.Interfaces.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        Task AddAsync(Department department);
+        Task<Department?> GetByIdAsync(int id);
+        Task<List<Department>> GetAllAsync();
+    }
+}
---
+++ Infra.Persistence/Services/Repositories/DepartmentRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.Contracts.Interfaces.Repositories;
+using Domain.Entities.HumanResources.EmployeeManagement;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+using Persistence.Services.Repository;
+
+namespace Persistence.Services.Repositories
+{
+    public class DepartmentRepository : GenericRepository<Department>,IDepartmentRepository
+    {
+        private readonly SafirDbContext _context;
+        public DepartmentRepository(SafirDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Department department)
+        {
+            await _context.Departments.AddAsync(department);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Department?> GetByIdAsync(int id)
+        {
+            return await _context.Departments.FindAsync(id);
+        }
+
+        public async Task<List<Department>> GetAllAsync()
+        {
+            return await _context.Departments.Where(d=>!d.IsDelete).ToListAsync();
+        }
+    }
+}
