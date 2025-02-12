63243b314557e1d3dd5dd2cdfcfa6b6b85f771b9
Masoud Kian <masoudkiannezhad@gmail.com>
2025-02-13 02:08:37 +0330

    Start Add Position

---
+++ WEB/wwwroot/admin/dist/js/utils/Sidebar.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const menuLinks = document.querySelectorAll(".side-menu__link");
+    const currentPath = window.location.pathname; // آدرس صفحه‌ی فعلی
+
+    menuLinks.forEach(link => {
+        const linkPath = link.getAttribute("href");
+
+        if (linkPath === currentPath) {
+            link.classList.add("side-menu__link--active");
+        } else {
+            link.classList.remove("side-menu__link--active");
+        }
+
+        link.addEventListener("click", function () {
+            menuLinks.forEach(item => item.classList.remove("side-menu__link--active"));
+            this.classList.add("side-menu__link--active");
+        });
+    });
+});
