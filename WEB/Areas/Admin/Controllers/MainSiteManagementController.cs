10f939a917cdaf64cc5a1f1e60dc0169805f96f1
Masoud Kian <masoudkiannezhad@gmail.com>
2025-02-13 06:28:27 +0330

    CRM Area Panel

---
+++ WEB/Areas/Admin/Controllers/MainSiteManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Areas.Admin.Controllers
+{
+    public class MainSiteManagementController : AdminBaseController
+    {
+        public IActionResult SiteUserList()
+        {
+            return View();
+        }
+    }
+}
