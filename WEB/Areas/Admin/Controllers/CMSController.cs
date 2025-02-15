d1b48a649721e41a2d6e68fcf084916d23ae74df
Masoud Kian <masoudkiannezhad@gmail.com>
2025-02-15 10:35:45 +0330

    set user list controller & view

---
+++ WEB/Areas/Admin/Controllers/CMSController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Areas.Admin.Controllers
+{
+    public class CMSController : AdminBaseController
+    {
+
+        #region User Management
+
+        [HttpGet("user-list")]
+        public IActionResult SiteUserList()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+        #region Role Management
+
+        [HttpGet("role-list")]
+        public IActionResult SiteRoleList()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
